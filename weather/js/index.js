b5923f8b1dd851619effac2c80222236955b93d9
Athilathesni <usmanchusman606@gmail.com>
2024-10-16 13:37:26 +0530

    file added

--- weather/js/index.js
+++ weather/js/index.js
@@ -31,6 +31,44 @@ else if(data.weather[0].main=="rain"){
 document.querySelector(".weather").style.display="block"
 
 }
+
+
 searchBtn.addEventListener("click",()=>{
     checkweather(searchBox.value)
 })
+
+
+
+
+// const weatherData = {
+//     city: "New York",
+//     temperature: 20,
+//     condition: "Clear",
+// };
+
+// function changeBackground(condition) {
+//     const body = document.body;
+//     body.className = ''; // Clear previous classes
+
+//     if (condition.toLowerCase() === "clear") {
+//         body.classList.add('clear');
+//     } else if (condition.toLowerCase() === "rain") {
+//         body.classList.add('rain');
+//     } else if (condition.toLowerCase() === "cloude") {
+//         body.classList.add('cloude');
+//     }
+// }
+
+// // Display weather data and set background
+// document.getElementById('city').textContent= weatherData.city;
+// document.getElementById('temperature').innerHTML = `Temperature: ${weatherData.temperature}°C`;
+// document.getElementById('condition').innerHTML = `Condition: ${weatherData.condition}`;
+// changeBackground(weatherData.condition);
+
+
+
+
+
+
+
+
